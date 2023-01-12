f5d4df5a9202de4d8b46b9dfc9587e6a2a4a3903
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-01-12 20:20:13 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("#search-box");
-
+const list = document.querySelector(".country-list");
+//Test
+console.log(list);
