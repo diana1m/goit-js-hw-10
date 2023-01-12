8636e10374e3955a527555985529f50f0e008656
diana1m <diana.maliarchuk.work@gmail.com>
2023-01-12 22:16:59 +0000

    Deploying to gh-pages from @ diana1m/goit-js-hw-10@aea78cc1805c4a6d6c64cfec028550f8ae10630b 🚀

--- index.4c6670ed.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box");const e=document.querySelector(".country-list");console.log(e);
-//# sourceMappingURL=index.4c6670ed.js.map
--- index.5daa9e38.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("#search-box");var e=document.querySelector(".country-list");console.log(e)}();
-//# sourceMappingURL=index.5daa9e38.js.map
